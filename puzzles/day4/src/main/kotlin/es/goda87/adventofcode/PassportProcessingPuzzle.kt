e911eacdda90f7544f37f22668d1032397280f4c
goda87 <goda87@gmail.com>
2020-12-13 23:54:13 +0100

    day4 p2 test pass

--- puzzles/day4/src/main/kotlin/es/goda87/adventofcode/PassportProcessingPuzzle.kt
+++ puzzles/day4/src/main/kotlin/es/goda87/adventofcode/PassportProcessingPuzzle.kt
@@ -31,7 +31,63 @@ class PassportProcessingPuzzle(private val validateData: Boolean = false) : Puzz
     }
 
     private fun isValidPassport(passport: CharSequence): Int {
-        val keys = passport.split("\n", " ").map { it.split(":")[0] }
-        return if (keys.containsAll(mandatoryKeycodes)) 1 else 0
+        val tokens = passport.split("\n", " ")
+        val keys = tokens.map { it.split(":")[0] }
+        if (!keys.containsAll(mandatoryKeycodes)) return 0
+        if (!validateData) {
+            return 1
+        }
+        val pairs = tokens.map { token -> token.split(":").let { it[0] to it[1] } }
+        val validData = pairs.map {
+            when (it.first) {
+                "byr" -> isValidByr(it.second)
+                "iyr" -> isValidIyr(it.second)
+                "eyr" -> isValidEyr(it.second)
+                "hgt" -> isValidHgt(it.second)
+                "hcl" -> isValidHcl(it.second)
+                "ecl" -> isValidEcl(it.second)
+                "pid" -> isValidPid(it.second)
+                "cid" -> true
+                else -> true
+            }
+        }.reduce { result, it -> result and it }
+        return if (validData) 1 else 0
     }
+
+    private fun isValidByr(byr: String): Boolean = try {
+        byr.toInt() in 1920..2002
+    } catch (e: Exception) {
+        false
+    }
+
+    private fun isValidIyr(iyr: String): Boolean = try {
+        iyr.toInt() in 2010..2020
+    } catch (e: Exception) {
+        false
+    }
+
+    private fun isValidEyr(eyr: String): Boolean = try {
+        eyr.toInt() in 2020..2030
+    } catch (e: Exception) {
+        false
+    }
+
+    private fun isValidHgt(hgt: String): Boolean = try {
+        val unit = hgt.substring(hgt.length - 2)
+        when (unit) {
+            "cm" -> hgt.subSequence(0, hgt.length - 2).toString().toInt() in 150..193
+            "in" -> hgt.subSequence(0, hgt.length - 2).toString().toInt() in 59..76
+            else -> false
+        }
+    } catch (e: Exception) {
+        false
+    }
+
+    private fun isValidHcl(hcl: String): Boolean = "^#[a-fA-F0-9]{6}\$".toRegex().matches(hcl)
+
+    private fun isValidEcl(ecl: String): Boolean =
+        listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(ecl)
+
+    private fun isValidPid(pid: String): Boolean = "^[0-9]{9}\$".toRegex().matches(pid)
+
 }
