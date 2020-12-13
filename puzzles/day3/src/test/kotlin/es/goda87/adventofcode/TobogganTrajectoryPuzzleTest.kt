df5130ac61b4defe332fd48eea246c023cdfde5f
goda87 <goda87@gmail.com>
2020-12-13 13:17:41 +0100

    parameterizedTest for TobogganTrajectory

--- puzzles/day3/src/test/kotlin/es/goda87/adventofcode/TobogganTrajectoryPuzzleTest.kt
+++ puzzles/day3/src/test/kotlin/es/goda87/adventofcode/TobogganTrajectoryPuzzleTest.kt
@@ -1,7 +1,13 @@
 package es.goda87.adventofcode
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 internal class TobogganTrajectoryPuzzleTest {
 
@@ -17,9 +23,24 @@ internal class TobogganTrajectoryPuzzleTest {
             "#...##....#\n" +
             ".#..#...#.#"
 
-    @Test
-    fun getResult() {
-        val actual = TobogganTrajectoryPuzzle().getResult(input)
-        assertEquals("7", actual)
+    @ParameterizedTest
+    @ArgumentsSource(TobogganTrajectoryPuzzleTestArgumentsProvider::class)
+    fun getResult(right: Int, down: Int, expected: String) {
+        val actual = TobogganTrajectoryPuzzle(right, down).getResult(input)
+        assertEquals(expected, actual)
     }
+}
+
+internal class TobogganTrajectoryPuzzleTestArgumentsProvider : ArgumentsProvider {
+
+    override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+        return Stream.of(
+            arguments(1,1,"2"),
+            arguments(3,1,"7"),
+            arguments(5,1,"3"),
+            arguments(7,1,"4"),
+            arguments(1,2,"2")
+        )
+    }
+
 }
