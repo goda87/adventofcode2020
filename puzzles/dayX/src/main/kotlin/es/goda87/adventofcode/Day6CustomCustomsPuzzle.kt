4730e89245db5a9fda5fdb1eb0ca6e5572e67fff
goda87 <goda87@gmail.com>
2020-12-14 18:32:06 +0100

    day6 p2 test pass

--- puzzles/dayX/src/main/kotlin/es/goda87/adventofcode/Day6CustomCustomsPuzzle.kt
+++ puzzles/dayX/src/main/kotlin/es/goda87/adventofcode/Day6CustomCustomsPuzzle.kt
@@ -16,5 +16,7 @@ class Day6P2CustomCustomsPuzzle : Puzzle {
     override fun getDefinition(): CharSequence = "https://adventofcode.com/2020/day/6"
 
     override fun getResult(input: CharSequence): String =
-        TODO("Not implemented")
+        input.split("\n\n").map { it.split("\n").map { it.toList() } }
+            .map { it.reduce { acc, list -> acc.intersect(list).toList() } }.map { it.size }
+            .reduce { acc, i -> acc + i }.toString()
 }
