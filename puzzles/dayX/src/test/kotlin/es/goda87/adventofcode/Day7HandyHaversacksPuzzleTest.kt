f78e91e94e44a7f0894ec9a1351b84a63f436901
goda87 <goda87@gmail.com>
2020-12-19 17:48:58 +0100

    day7 p2 test fail

--- puzzles/dayX/src/test/kotlin/es/goda87/adventofcode/Day7HandyHaversacksPuzzleTest.kt
+++ puzzles/dayX/src/test/kotlin/es/goda87/adventofcode/Day7HandyHaversacksPuzzleTest.kt
@@ -5,15 +5,27 @@ import org.junit.jupiter.api.Test
 
 internal class Day7HandyHaversacksPuzzleTest {
 
-    private val input = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
-            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
-            "bright white bags contain 1 shiny gold bag.\n" +
-            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
-            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
-            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
-            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
-            "faded blue bags contain no other bags.\n" +
-            "dotted black bags contain no other bags."
+    private val input = """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+    """.trimIndent()
+
+    private val alternateInput = """
+        shiny gold bags contain 2 dark red bags.
+        dark red bags contain 2 dark orange bags.
+        dark orange bags contain 2 dark yellow bags.
+        dark yellow bags contain 2 dark green bags.
+        dark green bags contain 2 dark blue bags.
+        dark blue bags contain 2 dark violet bags.
+        dark violet bags contain no other bags.
+    """.trimIndent()
 
     @Test
     fun getResult() {
@@ -21,9 +33,14 @@ internal class Day7HandyHaversacksPuzzleTest {
         assertEquals("4", actual)
     }
 
-//    @Test //TODO puzzle not defined yet
-//    fun part2getResult() {
-//        val actual = Day7p2HandyHaversacksPuzzle().getResult(input)
-//        assertEquals("6", actual)
-//    }
+    @Test
+    fun part2getResult() {
+        val actual = Day7p2HandyHaversacksPuzzle().getResult(input)
+        assertEquals("32", actual)
+    }
+    @Test
+    fun part2alternateGetResult() {
+        val actual = Day7p2HandyHaversacksPuzzle().getResult(alternateInput)
+        assertEquals("126", actual)
+    }
 }
